f7cba4be3677e740a1f7672eadc9d60113b0bad7
SamaelHugo <aubekirov.2@gmail.com>
2026-03-09 14:54:30 +0500

    Authentication

---
+++ src/pages/login.tsx
@@ -0,0 +1,20 @@
+import {auth, provider} from "../config/firebase"
+import {signInWithPopup} from "firebase/auth"
+import {useNavigate} from "react-router-dom"
+
+export const Login = () => {
+    const navigate = useNavigate()
+
+    const signInWithGoogle = async () => {
+        const result = await signInWithPopup(auth, provider)
+        console.log(result);
+        navigate("/")
+    }
+
+    return (
+        <div>
+            <h4>Sign in with Google to continue </h4>
+            <button onClick={signInWithGoogle} >Sign in with Google</button>
+        </div>
+    )
+}
