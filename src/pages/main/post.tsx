9cd80ec94f5f3f9d14cec703dab5d923fbc7fb8c
SamaelHugo <samaelhugo@users.noreply.github.com>
2026-03-11 21:45:03 +0500

    added ability to like post

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,14 +1,52 @@
 import {PostType} from "./types";
+import {addDoc, getDocs, collection, query, where} from "firebase/firestore";
+import {auth, db} from "../../config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useEffect, useState} from "react";
 
 interface Props {
  post : PostType
 }
 
+interface Like {
+    userId: string
+}
+
+
 export const Post = (props: Props) => {
     const {post} = props
+    const [user] = useAuthState(auth)
+
+    const [likes, setLikes] = useState<Like[] | null>(null)
+
+    const likesRef = collection(db, "likes")
+    const likesDoc = query(likesRef, where("postId", "==", post.id))
+
+    const getLikes = async () => {
+        const data = await getDocs(likesDoc)
+        setLikes(data.docs.map((doc) => ({userId: doc.data().userId})))
+    }
+
+    useEffect(() => {
+        getLikes()
+    }, [])
+
+    const addLike = async () => {
+        await addDoc(likesRef, {
+            userId: user?.uid,
+            postId: post.id
+        })
+    }
+
+    const hasUserLiked = likes?.find((like) => like.userId === user?.uid)
+
     return <div>
         <div className="title"><h1>{post.title}</h1></div>
         <div className="body"><p>{post.description}</p></div>
-        <div className="footer"><p>@{post.username}</p></div>
+        <div className="footer">
+            <p>@{post.username}</p>
+            <button onClick={addLike}>{hasUserLiked ? <>&#128078;</> : <>&#128077;</>} </button>
+            {likes && <p>Likes: {likes?.length}</p>}
+        </div>
     </div>
 }
